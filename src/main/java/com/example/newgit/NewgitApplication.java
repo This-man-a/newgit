e26f9a25c5d82bd55e0f3bf302f90fdfd51c1a97
snl <snl@163.com>
2020-03-09 02:00:04 +0800

    init

---
+++ src/main/java/com/example/newgit/NewgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.newgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewgitApplication.class, args);
+    }
+
+}
